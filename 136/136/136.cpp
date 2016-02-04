5eabc6bb5e5a5c00424c9d41b958e19f437ca5fb
Mike <Mike.Zhang@maplesfs.com>
2016-02-04 13:30:27 -0500

    $1

--- 136/136/136.cpp
+++ 136/136/136.cpp
@@ -1,9 +1,29 @@
 #include "stdafx.h"
 
+int g(int x);
+int h(int x);
+
+int f(int x) {
+	printf("f(%d) called\n", x);
+	int y = g(x);
+	y += h(g(x - 1));
+	return y;
+}
+
+int g(int x) {
+	printf("g(%d) called\n", x);
+	int y = h(x * 5);
+	return y;
+}
+
+int h(int x) {
+	printf("h(%d) called\n", x);
+	int y = x * x;
+	return y / 2;
+}
+
+
 int _tmain(int argc, _TCHAR* argv[]){
-
-	telegram();
-	getchar();
-	getchar();
-
+	int x = f(5);
+	printf("x = %d\n", x);
 }
